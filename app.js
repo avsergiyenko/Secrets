053ba4063dba97d6bf878c85927dba82623a5a8e
Artem Sergiyenko <avsergiyenko@gmail.com>
2021-07-20 05:08:08 +0300

    Adding and implementing bcrypt + Salting

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -16,7 +16,6 @@ const userSchema = new mongoose.Schema ({
     password: String,
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -39,18 +38,21 @@ app.get("/register", (req,res) => {
 
 app.post("/register", (req,res) => {
 
-    const newUser = new User ({
-        email: req.body.username,
-        password: req.body.password
-    });
-    
-    newUser.save((err) => {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    });
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+        
+        newUser.save((err) => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
+    })
 });
 
 app.post("/login", (req,res) => {
@@ -62,9 +64,11 @@ app.post("/login", (req,res) => {
             console.log(err);
         } else {
             if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if(result === true) {
+                        res.render("secrets");
+                    }
+                });             
             }
         }
     });
